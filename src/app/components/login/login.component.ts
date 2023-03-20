555139d586405b04a3f49306325fc1ea9407b16a
joel <joelth368@gmail.com>
2023-03-20 12:31:47 +0100

    Componente Login finalizado (falta autenticacion)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+
+  ngOnInit() {
+  }
+
+  getEmail() {
+    return this.loginForm.get('email');
+  }
+
+  getPassword() {
+    return this.loginForm.get('password');
+  }
 
 }
